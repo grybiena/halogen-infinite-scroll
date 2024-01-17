8c14423c6e05dea645a2e6fcdc8a7b8f692690ab
grybiena <135348827+grybiena@users.noreply.github.com>
2024-01-18 00:09:39 +0100

    refactor event handling to remove mask (#2)
    * refactor event handling to remove mask
    
    see "Suspicious design elements" referenced here https://github.com/grybiena/halogen-infinite-scroll/issues/1
    
    * put sign back to +
    
    ---------
    
    Co-authored-by: f <f@grybiena.com>

--- src/Halogen/Infinite/Scroll.purs
+++ src/Halogen/Infinite/Scroll.purs
@@ -5,8 +5,10 @@ import Prelude hiding (top, bottom)
 import CSS (height, marginRight, opacity, paddingRight, pct, position, relative, top, width, zIndex)
 import CSS.Overflow (overflow, overflowY, scroll, hidden, overflowAuto)
 import CSS.Size (px)
+import Control.Monad.Free (Free, liftF, runFreeM)
 import Control.Monad.Rec.Class (tailRecM, Step(..))
 import Control.Monad.Resource (Resource, runResource)
+import Control.Monad.State (State, execState, modify_)
 import Data.Array (cons, fold, head, last, null)
 import Data.Array as A
 import Data.List as List
@@ -59,6 +61,7 @@ type FeedParams e =
   , pageSize :: Int
   , hiddenPages :: Int
   , preloadedPages :: Int
+  , enableTop :: Boolean
   , preloadMillis :: Number
   }
 
@@ -68,6 +71,7 @@ defaultFeedParams e =
   , pageSize: 10
   , hiddenPages: 3
   , preloadedPages: 3 
+  , enableTop: true
   , preloadMillis: 333.3
   }
 
@@ -84,6 +88,7 @@ component =
                           , pages: Map.empty 
                           , preloaded: Map.empty
                           , lock: Nothing
+                          , update: { topLoad: Nothing, bottomLoad: Nothing, scroll: Nothing }
                           }
     , render: renderFeed
     , eval: H.mkEval $ H.defaultEval { handleAction = handleFeedAction
@@ -125,6 +130,11 @@ type FeedState e =
   , pages :: Map Int (Page e)
   , preloaded :: Map Int (Page e)
   , lock :: Maybe (AVar Unit)
+  , update :: {
+        topLoad :: Maybe { key :: Int, value :: Page e }
+      , bottomLoad :: Maybe { key :: Int, value :: Page e }
+      , scroll :: Maybe Number
+      }
   }
 
 data FeedAction e =
@@ -186,14 +196,20 @@ handleFeedAction (PageOutput (PageHeight {id, height})) = do
                           }
     setHeight Nothing = Nothing
     setHeight (Just page) = Just (page { pageHeight = height })
-handleFeedAction (PageOutput (PageIntersection {id, height})) = do
-  H.modify_ setPageIntersection
-  H.getRef (H.RefLabel "feed") >>= traverse_ manageFeed
-  where
-    setPageIntersection st = st { pages = Map.alter setIntersection id st.pages
-                                }
-    setIntersection Nothing = Nothing
-    setIntersection (Just page) = Just (page { visibleHeight = height })
+handleFeedAction (PageOutput (PageIntersection page)) = mask do
+  feed' <- H.getRef (H.RefLabel "feed")
+  flip traverse_ feed' $ \feed -> do  
+    st <- H.modify (\st -> execState (stateUpdate (computeUpdate st page)) st) 
+    t <- H.liftEffect $ scrollTop feed
+    H.liftEffect $ traverse_ (\o -> setScrollTop (t + o) feed) st.update.scroll
+    H.modify_ (\s -> s { update = { topLoad: Nothing, bottomLoad: Nothing, scroll: Nothing } })
+    flip traverse_ st.update.topLoad $ \kv -> do
+      a <- loadPageAbove feed kv.value
+      traverse_ (\p -> H.modify_ (\s -> s { preloaded = Map.insert p.id p st.preloaded } )) a
+    flip traverse_ st.update.bottomLoad $ \kv -> do
+      a <- loadPageBelow feed kv.value
+      traverse_ (\p -> H.modify_ (\s -> s { pages = Map.insert p.id p s.pages } )) a
+
 handleFeedAction (PageOutput (PageEmpty i)) = do
   H.modify_ (\st -> st { pages = Map.delete i st.pages })
 
@@ -214,61 +230,119 @@ managePreload feed = do
                                             (Map.findMin st.preloaded)
                          })
 
-manageFeed :: forall e o m .
-              Feed e m
-           => Element -> H.HalogenM (FeedState e) (FeedAction e) (FeedSlots e) o m Unit
-manageFeed feed = mask do
-  manageAbove
-  manageBelow
-  managePreload feed
+data UpdateF e a =
+    TopShift { key :: Int, value :: Page e } a 
+  | TopUnshift { key :: Int, value :: Page e } a
+  | TopLoad { key :: Int, value :: Page e } a
+  | TopDelete { key :: Int, value :: Page e } a 
+  | BottomLoad { key :: Int, value :: Page e } a
+  | BottomDelete { key :: Int, value :: Page e } a
+  | UpdateIntersection PageIntersection a
+
+instance Functor (UpdateF e) where
+  map f (TopShift page a) = TopShift page (f a)
+  map f (TopUnshift page a) = TopUnshift page (f a)
+  map f (TopLoad page a) = TopLoad page (f a)
+  map f (TopDelete page a) = TopDelete page (f a)
+  map f (BottomLoad page a) = BottomLoad page (f a)
+  map f (BottomDelete page a) = BottomDelete page (f a)
+  map f (UpdateIntersection page a) = UpdateIntersection page (f a)
+
+type Update e = Free (UpdateF e)
+
+topShift :: forall e. { key :: Int, value :: Page e } -> Update e Unit
+topShift page = liftF $ TopShift page unit
+
+topUnshift :: forall e. { key :: Int, value :: Page e } -> Update e Unit
+topUnshift page = liftF $ TopUnshift page unit
+
+topLoad :: forall e. { key :: Int, value :: Page e } -> Update e Unit
+topLoad page = liftF $ TopLoad page unit
+
+topDelete :: forall e. { key :: Int, value :: Page e } -> Update e Unit
+topDelete page = liftF $ TopDelete page unit
+ 
+bottomLoad :: forall e. { key :: Int, value :: Page e } -> Update e Unit
+bottomLoad page = liftF $ BottomLoad page unit
+
+bottomDelete :: forall e. { key :: Int, value :: Page e } -> Update e Unit
+bottomDelete page = liftF $ BottomDelete page unit
+
+updateIntersection :: forall e. PageIntersection -> Update e Unit
+updateIntersection page = liftF $ UpdateIntersection page unit
+
+stateUpdate :: forall e. Update e Unit -> State (FeedState e) Unit
+stateUpdate f = do
+  modify_ (\st -> st { update = st.update { scroll = Nothing } })
+  runFreeM go f
   where
-    manageBelow = do
-      state <- H.get
-      let occluded (Tuple _ page) = page.visibleHeight == 0.0 
-          extra = A.length $ A.takeWhile occluded $ A.reverse $ Map.toUnfoldable state.pages
-          target =  state.feedParams.hiddenPages + state.feedParams.preloadedPages
-          tooFew = extra < target
-          tooMany = extra > target
-      when tooFew do
-        a <- join <$> traverse (\kv -> loadPageBelow feed kv.value) (Map.findMax state.pages)
-        traverse_ (\p -> H.modify_ (\st -> st { pages = Map.insert p.id p st.pages } )) a
-      when tooMany do
-        H.modify_ (\st -> st { pages = maybe st.pages
-                                             (\kv -> Map.delete kv.key st.pages)
-                                             (Map.findMax st.pages)
-                             })
-     
-    manageAbove = do
-      st <- H.get
-      let occluded (Tuple _ page) = page.visibleHeight == 0.0 
-          extra = A.length $ A.takeWhile occluded $ Map.toUnfoldable st.pages
-          tooFew = extra < st.feedParams.hiddenPages
-          tooMany = extra > st.feedParams.hiddenPages
-      when tooFew do
-        traverse_ loadTop (Map.findMax st.preloaded) 
-      when tooMany do
-        traverse_ unloadtop (Map.findMin st.pages)
-        where
-
-          loadTop page = do
-            t <- H.liftEffect $ scrollTop feed
-            H.modify_ (\st -> st { 
-                         pages = Map.insert page.key page.value st.pages 
-                       , preloaded = Map.delete page.key st.preloaded
-                       } )
-            H.liftEffect $ setScrollTop (t + page.value.pageHeight) feed
-
-          unloadtop page = do
-            t <- H.liftEffect $ scrollTop feed
-            H.modify_ (\st -> st { 
-                         pages = Map.delete page.key st.pages
-                       , preloaded = Map.insert page.key page.value st.preloaded
-                       } )
-            H.liftEffect $ setScrollTop (t - page.value.pageHeight) feed
-
+    go (TopShift page a) = do 
+      modify_ (\st -> st { 
+          pages = Map.insert page.key page.value st.pages 
+        , preloaded = Map.delete page.key st.preloaded
+        , update = st.update { scroll = Just page.value.pageHeight }
+        } )
+      pure a
+    go (TopUnshift page a) = do
+      modify_ (\st -> st { 
+          pages = Map.delete page.key st.pages
+        , preloaded = Map.insert page.key page.value st.preloaded
+        , update = st.update { scroll = Just (-page.value.pageHeight) }
+        })
+      pure a
+    go (TopLoad page a) = do
+      modify_ (\st -> st {
+          update = if ((\u -> u.key) <$> st.update.topLoad) == Just page.key
+                     then st.update { topLoad = Nothing }
+                     else st.update { topLoad = Just page }
+        })
+      pure a
+    go (TopDelete page a) = do 
+      modify_ (\st -> st {
+          preloaded = Map.delete page.key st.preloaded
+        , update = st.update { topLoad = Nothing }
+        })
+      pure a
+    go (BottomLoad page a) = do
+      modify_ (\st -> st {
+          update = if ((\u -> u.key) <$> st.update.bottomLoad) == Just page.key
+                     then st.update { bottomLoad = Nothing }
+                     else st.update { bottomLoad = Just page }
+        })
+      pure a
+    go (BottomDelete page a) = do 
+      modify_ (\st -> st {
+          pages = Map.delete page.key st.pages
+        , update = st.update { bottomLoad = Nothing }
+        })
+      pure a 
+    go (UpdateIntersection { id, height } a) = do 
+      let setIntersection Nothing = Nothing
+          setIntersection (Just page) = Just (page { visibleHeight = height })
+      modify_ (\st -> st {
+          pages = Map.alter setIntersection id st.pages
+        })
+      pure a
     
-      
-
+computeUpdate :: forall e. FeedState e -> PageIntersection -> Update e Unit
+computeUpdate { feedParams, pages, preloaded } page = 
+  let notVisible (Tuple _ p) = p.visibleHeight == 0.0 
+      extraAbove = A.length $ A.takeWhile notVisible $ Map.toUnfoldable pages
+      tooFewAbove = extraAbove < feedParams.hiddenPages
+      tooManyAbove = extraAbove > feedParams.hiddenPages
+      tooFewPreload = Map.size preloaded < feedParams.preloadedPages 
+      tooManyPreload = Map.size preloaded > feedParams.preloadedPages 
+      extraBelow = A.length $ A.takeWhile notVisible $ A.reverse $ Map.toUnfoldable pages
+      tooFewBelow = extraBelow < feedParams.hiddenPages
+      tooManyBelow = extraBelow > feedParams.hiddenPages
+   in do
+    updateIntersection page
+    when (feedParams.enableTop && tooFewAbove) $ traverse_ topShift (Map.findMax preloaded)
+    when (feedParams.enableTop && tooManyAbove) $ traverse_ topUnshift (Map.findMin pages)
+    when (feedParams.enableTop && tooFewPreload) $ traverse_ topLoad (Map.findMin preloaded)
+    when (feedParams.enableTop && tooManyPreload) $ traverse_ topDelete (Map.findMin preloaded)
+    when tooFewBelow $ traverse_ bottomLoad (Map.findMax pages)
+    when tooManyBelow $ traverse_ bottomDelete (Map.findMax pages)
 
 
 mask :: forall e o m .
@@ -422,15 +496,18 @@ data PageAction e =
   | PageIntersect (Array IntersectionObserverEntry)
   | PageDeleteElement e
 
+type PageIntersection = {
+      id :: Int
+    , height :: Number
+    } 
+
+
 data PageOutput =
     PageHeight {
       id :: Int
     , height :: Number
     }
-  | PageIntersection {
-      id :: Int
-    , height :: Number
-    } 
+  | PageIntersection PageIntersection
   | PageEmpty Int
 
 type PageSlots e = ( pageElement :: forall q o . H.Slot q o e )
@@ -489,6 +566,7 @@ pageComponent =
     pageAction (PageIntersect o) = do
       let h = sum ((\e -> e.intersectionRect.height) <$> o)
       pageId <- H.gets (\page -> page.id)      
+      H.modify_ (\st -> st { visibleHeight = h})
       H.raise (PageIntersection { id: pageId, height: h })
     pageAction (PageDeleteElement e) = do
       state <- H.modify (\st -> st { elements = A.delete e st.elements })
